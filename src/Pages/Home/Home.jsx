c843ad5c7a8cb5dfd575e4b0099962aa1a21e71e
Bisratolera <leultolera@gmail.com>
2024-12-29 23:50:01 +0300

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Home = () => {
+  return (
+    <>
+      <div className="flex text-4xl justify-center items-center h-screen bg-blue-950">
+        <h1>Say Hello To Home Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;  
